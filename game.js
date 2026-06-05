23e14728acb568fd3b132fdad11295bcece8457b
eduardoquispe <eduardoquispe344@gmail.com>
2026-06-04 19:25:48 -0500

    fix: prevent further actions after game over

--- game.js
+++ game.js
@@ -146,6 +146,7 @@ function spawn() {
   next = randomPiece();
   if (collide(current.shape, current.x, current.y)) {
     endGame();
+    return;
   }
   drawNext();
 }
@@ -256,6 +257,7 @@ function loop(ts) {
       lockPiece();
     }
   }
+  if (gameOver) return;
   draw();
   animId = requestAnimationFrame(loop);
 }
